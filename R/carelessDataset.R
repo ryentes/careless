70e34926f71274f2e721231ac21a9b07c7baf1d7
fwilhelm <franciscowilhelm@users.noreply.github.com>
2018-05-23 09:50:41 +0200

    typos and renamed carelessDataset columns
    - typo in IRV / psacing in psychsyn
    - carelessDataset now has cogent column names

--- R/carelessDataset.R
+++ R/carelessDataset.R
@@ -3,7 +3,7 @@
 #' A simulated dataset mimicking insufficient effort responding. Contains three types of responses:
 #' (a) Normal responses with answers centering around a trait/attitude value
 #' (80 percent probability per simulated observation),
-#' (b)Straightlining responses (10 percent probability per simulated observation),
+#' (b) Straightlining responses (10 percent probability per simulated observation),
 #' (c) Random responses (10 percent probability per simulated observation).
 #' Simulated are 10 subscales of 5 items each (= 50 variables).
 #'

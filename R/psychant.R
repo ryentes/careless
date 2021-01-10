8b5416c9887f811cdaa39a0016af454f3e7ade7f
Francisco Wilhelm <38857150+franciscowilhelm@users.noreply.github.com>
2021-01-10 17:38:13 +0100

    Revert "Revert "Merge branch 'master' into dev""
    This reverts commit 4ff80a3dff9f1baa9630f0d7ca7615d212e36875.

--- R/psychant.R
+++ R/psychant.R
@@ -6,7 +6,7 @@
 #' @param critval is the minimum magnitude of the correlation between two items
 #' in order for them to be considered psychometric synonyms. Defaults to -.60
 #' @param diag additionally return the number of item pairs available for each subject. Useful if dataset contains many missing values.
-#' @author Richard Yentes \email{rdyentes@ncsu.edu}, Francisco Wilhelm \email{franciscowilhelm@gmail.com}
+#' @author Richard Yentes \email{ryentes@gmail.com}, Francisco Wilhelm \email{franciscowilhelm@gmail.com}
 #' @export
 #' @seealso \code{\link{psychsyn}} for the main function, \code{\link{psychsyn_critval}} for a helper that allows to set an
 #' adequate critical value for the size of the correlation.
---
+++ cran-comments.md
@@ -0,0 +1,10 @@
+## Test environments
+
+## R CMD check results
+There were no ERRORs or WARNINGs. 
+
+There was 1 NOTE:
+
+
+## Downstream dependencies
+None

051dd982be654f975e7915176e1a57e626b4d6be
Richard Yentes <ryentes@gmail.com>
2021-04-25 12:39:37 -0400

    Update R/psychsyn_critval.R

--- R/psychsyn_critval.R
+++ R/psychsyn_critval.R
@@ -23,7 +23,7 @@ psychsyn_critval <- function(x, anto = FALSE) {
   else {
   correlations <- correlations[order(correlations$Freq, decreasing = FALSE),]
   }
-  names(correlations) <- c("Var1","Var2","Correlation")
+  names(correlations) <- c("var1","var2","cor")
   
   return(correlations)
 }

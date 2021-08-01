e6c31f210b9678d39f58b2b31993ac3d344cc83b
Richard Yentes <ryentes@gmail.com>
2021-08-01 00:31:00 -0400

    Update test-longstring.R

--- tests/testthat/test-longstring.R
+++ tests/testthat/test-longstring.R
@@ -1,6 +1,14 @@
-fname <- system.file("testdata", "lonstring-test-data.csv", package="careless")
-ls.test.data <- read.csv(fname, header=T)
+basic = as.data.frame(t(matrix(rep(seq(10), 10), ncol = 10, nrow = 10)))
+basic[lower.tri(basic, diag=TRUE)] = 1
 
+# Test that the longstring function returns as expected on a basic dataset.
 test_that("basic longstring output is correct", {
-  expect_identical(longstring(ls.test.data), c(1:9*10,10))
+  expect_identical(longstring(basic), as.numeric(seq(10)))
+})
+
+
+# Test that longstring handles NA Values in the NA test dataset.
+na_rows = c(103, 144, 138, 2, 194)
+test_that("longstring runs correctly on NA values", {
+  expect_identical(longstring(careless_testing_na[na_rows,]), c(1,4,5,4,3))
 })
